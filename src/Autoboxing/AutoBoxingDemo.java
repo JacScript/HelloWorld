743baab430ddc5537b71d14e762884061a7ff093
JacScript <jacksonsylas59@gmail.com>
2025-11-30 22:14:32 +0300

    synchronization

---
+++ src/Autoboxing/AutoBoxingDemo.java
@@ -0,0 +1,25 @@
+package Autoboxing;
+
+public class AutoBoxingDemo {
+    public static void main(String[] args){
+        int i = 0;
+
+        //Autobox
+        Integer iObj = Integer.valueOf(i);
+        System.out.println("Value of Integer Obj: " + iObj);
+
+        // auto-unbox
+        int i1 = iObj;
+        System.out.println("Value of i1: " + i1);
+
+        //AutoBox
+        Character  charObj = 'a';
+
+        //Auto-unBox
+        char ch = charObj;
+        System.out.println("Value of ch: " + ch);
+        System.out.println("Value of charObj" + charObj);
+    }
+
+
+}
